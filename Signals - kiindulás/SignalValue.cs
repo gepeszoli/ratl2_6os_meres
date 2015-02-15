c8b5cdd15400f511ff99421af18db20d29770d18
Gepesz Zoltan <gepeszoli@gmail.com>
2015-02-15 20:28:33 +0100

    removed Ticks

--- Signals - kiindulás/SignalValue.cs
+++ Signals - kiindulás/SignalValue.cs
@@ -9,7 +9,6 @@ namespace Signals
     {
         public double Value;
         public DateTime TimeStamp;
-        public long Ticks;
         public SignalValue (double value)
         {
             Value = value;
@@ -20,7 +19,6 @@ namespace Signals
             Value = value;
             timeStamp = timeStamp.ToUniversalTime();
             TimeStamp = timeStamp;
-            Ticks = TimeStamp.Ticks;
         }
         public override string ToString()
         {
